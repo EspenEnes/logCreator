4e372cc104984d64d7b6dd565753b8d7ccef2681
Espen Enes <espen.enes@nov.com>
2023-11-22 17:24:23 +0100

    bumped to rev 0.9

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class PlclogConfigCreator(QtWidgets.QMainWindow, PlcLogCreator.Ui_MainWindow):
         self.settings = QSettings()
 
         self.configText = ""
-        self.version = "0.8"
+        self.version = "0.9"
 
         self.actionLoad.setDisabled(True)
         self.actionSave_Config.setDisabled(True)
